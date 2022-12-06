39091c922c96012cdedcdc34c03f623a46dcc0dc
Andrew <pasynkov2010@icloud.com>
2022-12-06 15:38:33 +0400

    add close btn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import { useEffect } from 'react';
+
+const tg = window.Telegram.WebApp;
+
 function App() {
-  console.log(window.Telegram.WebApp.initData, 'asd');
-  return <div className="App">work</div>;
+  console.log(tg);
+
+  const onClose = () => {
+    tg.close();
+  };
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  return (
+    <div className="App">
+      work<button onClose={onClose}>Закрыть</button>
+    </div>
+  );
 }
 
 export default App;
