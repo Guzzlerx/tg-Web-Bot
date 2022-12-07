7b113f3aa72cfce72fb3088815d6dd672f7c9a46
Andrew <pasynkov2010@icloud.com>
2022-12-07 21:19:09 +0400

    add main page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,19 +4,45 @@ import Header from '../../components/Header/Header';
 
 import UseTelegram from '../../hooks/useTelegram';
 
+import saleIcon from '../../assets/SaleIcon.svg';
+import moneyBagIcon from '../../assets/moneyBagIcon.svg';
+
 import './MainPage.css';
 
 const MainPage = () => {
   const { user, closeApp } = UseTelegram();
 
   return (
-    <div className="page">
+    <div className="main">
       <Header />
-      <h1 className="title">
-        Надеюсь, что этот бот будет полезен и поможет вам как заработать, так и
-        сэкономить{' '}
-      </h1>
-      <button onClick={closeApp}>Закрыть</button>
+      <div className="container">
+        <h1 className="main__title">
+          Надеюсь, что этот бот будет полезен и поможет вам как заработать, так
+          и сэкономить{' '}
+        </h1>
+        <div className="main__hr" />
+        <p className="main__info">Используй кнопки ниже</p>
+        <div className="main__btn-container">
+          <button className="button button_size_big button_color_blue">
+            <h3 className="button__title">Холява и Скидки</h3>
+            <img className="button__icon" src={saleIcon} alt="sale icon" />
+          </button>
+          <button className="button button_size_big button_color_sand">
+            <h3 className="button__title">Схемы заработка</h3>
+            <img
+              className="button__icon"
+              src={moneyBagIcon}
+              alt="money bag icon"
+            />
+          </button>
+        </div>
+        <button className="button button_color_purple">
+          Бесплатные покупки на WB
+        </button>
+        <button className="button button_type_close" onClick={closeApp}>
+          Закрыть
+        </button>
+      </div>
     </div>
   );
 };
